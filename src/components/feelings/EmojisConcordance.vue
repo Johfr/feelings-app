34fc60eeaf3fe940f033b371ef472ee6963c0397
Johfr <johanne.assorin@hotmail.com>
2025-07-26 19:48:22 +0200

    feat(useEmoji): - aficher les emojis et leur correspondance en valeur

---
+++ src/components/feelings/EmojisConcordance.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useEmoji } from '@/composables/useEmoji'
+
+const sortedEmoji = useEmoji.sort((a,b) => b.value - a.value)
+</script>
+
+<template>
+  <section class="hidden mt-[50px] md:block md:md-0 md:w-[22%]">
+    <h3 class="title-h3 mb-5">Emojis reference</h3>
+    <table class="w-[100%] text-center">
+      <thead>
+        <tr>
+          <th class="font-medium">Valeur</th>
+          <th class="font-medium">Emojis</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(emoji, emojiIndex) of sortedEmoji" :key="emojiIndex" class="h-10">
+          <td class="font-medium">{{ emoji.value }}</td>
+          <td class="md:text-xl">{{ emoji.icon }}</td>
+        </tr>
+      </tbody>
+    </table>
+   </section>
+</template>
+
+<style scoped lang="scss">
+</style>
